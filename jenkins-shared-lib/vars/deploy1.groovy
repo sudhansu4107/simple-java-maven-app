ff9c8fe8a4a9bfab3a8baa45dfb8b7a74ab046eb
sudhansu4107 <goudakumarsudhansu@gmail.com>
2025-08-20 01:36:17 +0530

    shared_lib

--- jenkins-shared-lib/vars/deploy1.groovy
+++ jenkins-shared-lib/vars/deploy1.groovy
@@ -1,9 +1,7 @@
-def deployApp1()
-{
-    echo "Deployed -App1 into prod."
-}
-
-def deployApp2()
-{
-    echo "Deployed -App2 into prod."
+def call(String app = "App1") {
+    if (app == "App1") {
+        echo "Deployed -App1 into prod."
+    } else if (app == "App2") {
+        echo "Deployed -App2 into prod."
+    }
 }
