0ecbd419898e994dbee9d9626fdc00dd9f27fd39
sudhansu4107 <goudakumarsudhansu@gmail.com>
2025-08-20 00:59:25 +0530

    shared_lib

---
+++ jenkins-shared-lib/vars/deploy1.groovy
@@ -0,0 +1,9 @@
+def deployApp1()
+{
+    echo "Deployed -App1 into prod."
+}
+
+def deployApp2()
+{
+    echo "Deployed -App2 into prod."
+}
---
+++ jenkins-shared-lib/vars/greet.groovy
@@ -0,0 +1,4 @@
+def call(string name='sudhansu')
+{
+    echo "Hi ${name} , we are glad you joined as for the celebration !"
+}
